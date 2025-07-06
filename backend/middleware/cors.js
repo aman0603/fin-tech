74361faca3eff399514073786aac5293a0838ee2
Aman Paswan <amanpaswan464@gmail.com>
2025-07-06 21:59:45 +0530

    changes

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -2,8 +2,9 @@ const cors = require('cors');
 dotenv = require('dotenv');
 dotenv.config();
 
-const allowedOrigins = process.env.CORS_ORIGIN ? [process.env.CORS_ORIGIN] : ['http://localhost:3000'];
+// const allowedOrigins = process.env.CORS_ORIGIN ? [process.env.CORS_ORIGIN] : ['http://localhost:3000'];
 module.exports = cors({
-  origin: allowedOrigins,
+  origin: ['http://localhost:3000',                      // local dev
+    'https://finance-tracker-alpha-pearl.vercel.app' ], // your deployed frontend,
   credentials: true,
 });
